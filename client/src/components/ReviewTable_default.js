47079b79a436a141f8e3666bcdf0162f025210b5
민태희 <122341071+DaeGul2@users.noreply.github.com>
2025-02-24 11:46:26 +0900

    d
    d

--- client/src/components/ReviewTable_default.js
+++ client/src/components/ReviewTable_default.js
@@ -25,11 +25,10 @@ const ReviewTable = ({ reviews, isLoading }) => {
               <th style={{ border: "1px solid #dee2e6", padding: "10px" }}>리뷰</th>
               <th style={{ border: "1px solid #dee2e6", padding: "10px" }}>판단결과 (1~5)</th>
               <th style={{ border: "1px solid #dee2e6", padding: "10px" }}>비고</th>
-              <th style={{ border: "1px solid #dee2e6", padding: "10px" }}>미래고객가능성</th>
-              <th style={{ border: "1px solid #dee2e6", padding: "10px" }}>기존 고객여부</th>
+              <th style={{ border: "1px solid #dee2e6", padding: "10px" }}>기관 호감도</th>
+              <th style={{ border: "1px solid #dee2e6", padding: "10px" }}>기존 지원여부</th>
               <th style={{ border: "1px solid #dee2e6", padding: "10px" }}>장점</th>
               <th style={{ border: "1px solid #dee2e6", padding: "10px" }}>단점</th>
-              <th style={{ border: "1px solid #dee2e6", padding: "10px" }}>추가 요청</th>
             </tr>
           </thead>
           <tbody>
@@ -43,19 +42,16 @@ const ReviewTable = ({ reviews, isLoading }) => {
                 </td>
                 <td style={{ border: "1px solid #dee2e6", padding: "10px" }}>{formatValue(item.비고)}</td>
                 <td style={{ border: "1px solid #dee2e6", padding: "10px", fontWeight: "bold", color: "#17a2b8" }}>
-                  {formatValue(item.미래고객가능성)}
+                  {formatValue(item.호감도)}
                 </td>
                 <td style={{ border: "1px solid #dee2e6", padding: "10px", fontWeight: "bold", color: item.기존지원자 === 1 ? "#007bff" : "#dc3545" }}>
-                  {formatValue(item.기존고객) === "1" ? "기존고객" : "신규 고객"}
+                  {formatValue(item.기존지원자) === "1" ? "기존 지원자" : "신규 지원자"}
                 </td>
                 <td style={{ border: "1px solid #dee2e6", padding: "10px", textAlign: "left" }}>
                   {formatValue(item.장점)}
                 </td>
                 <td style={{ border: "1px solid #dee2e6", padding: "10px", textAlign: "left", color: "#dc3545" }}>
                   {formatValue(item.단점)}
-                </td>
-                <td style={{ border: "1px solid #dee2e6", padding: "10px", textAlign: "left", color: "#dc3545" }}>
-                  {formatValue(item.요청)}
                 </td>
               </tr>
             ))}
