027bdb9046bbd88b42d674c1e8b139e9dd1f6427
민태희 <alsxogml1234@gmail.com>
2025-03-06 21:09:17 +0900

    d
    d

--- data/util/make_statistics.py
+++ data/util/make_statistics.py
@@ -6,7 +6,7 @@ from openpyxl.chart.label import DataLabelList
 from openpyxl.utils.dataframe import dataframe_to_rows
 
 # 엑셀 파일 불러오기
-df = pd.read_excel('국가철도공단_gpt분석.xlsx')
+df = pd.read_excel('한국보건산업진흥원_gpt분석.xlsx')
 
 # '장점'과 '단점' 컬럼의 데이터 정제
 def clean_and_split(data):
@@ -17,12 +17,15 @@ def clean_and_split(data):
 
 df['장점'] = df['장점'].apply(clean_and_split)
 df['단점'] = df['단점'].apply(clean_and_split)
+df['기관인상'] = df['기관인상'].apply(clean_and_split)
 
 # '모르겠음' 포함한 전체 통계 계산 (엑셀 저장용)
 advantages = df['장점'].explode().reset_index(drop=True).value_counts().reset_index()
 advantages.columns = ['항목', '빈도']
 disadvantages = df['단점'].explode().reset_index(drop=True).value_counts().reset_index()
 disadvantages.columns = ['항목', '빈도']
+institutionImage = df['기관인상'].explode().reset_index(drop=True).value_counts().reset_index()
+institutionImage.columns = ['항목', '빈도']
 
 # '모르겠음' 제외한 데이터 (차트 및 그래프용)
 advantages_filtered = advantages[advantages['항목'] != '모르겠음'].copy()
@@ -33,6 +36,7 @@ wb = Workbook()
 ws1 = wb.active
 ws1.title = "장점"
 ws2 = wb.create_sheet(title="단점")
+ws3 = wb.create_sheet(title="기관인상")
 
 
 # 데이터를 시트에 기록 (모르겠음 포함)
@@ -40,9 +44,11 @@ for row in dataframe_to_rows(advantages, index=False, header=True):
     ws1.append(row)
 for row in dataframe_to_rows(disadvantages, index=False, header=True):
     ws2.append(row)
+for row in dataframe_to_rows(institutionImage, index=False, header=True):
+    ws3.append(row)    
 
 
 # 엑셀 파일 저장
-wb.save('국가철도공단_통계.xlsx')
+wb.save('한국보건산업진흥원_gpt분석_통계.xlsx')
 
 print("✅ 통계 및 차트 생성 완료!")
